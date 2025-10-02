396ff1020ddb05b499684cf9ad8a29856ef3f3e9
aminul721 <aminul721@gmail.com>
2025-10-02 16:00:04 +0600

    feat: not mandatory

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -47,18 +47,6 @@ const ContactUs = () => {
             </form>
 
             {/* Right: Google Map */}
-            {/* <div className="w-full h-[400px] rounded-lg overflow-hidden border">
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!2m12!3m9!1s0x3755c7af3ef0bd6f%3A0xbbb8e4b9c72b9974!2sDhaka%2C%20Bangladesh!5e0!3m2!1sen!2sbd!4v1657555299085"
-                    width="100%"
-                    height="100%"
-                    allowFullScreen=""
-                    loading="lazy"
-                    className="w-full h-full"
-                    referrerPolicy="no-referrer-when-downgrade"
-                ></iframe>
-            </div> */}
-
             <div className="w-full h-[400px] rounded-lg overflow-hidden border">
                 <iframe 
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d115201.64749869412!2d89.60208!3d25.64724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e2dd7c1b4c5555%3A0x3d3f4d5e5a5b5c5d!2sPhulchhari%2C%20Gaibandha%2C%20Bangladesh!5e0!3m2!1sen!2sbd!4v1696234567890!5m2!1sen!2sbd"
